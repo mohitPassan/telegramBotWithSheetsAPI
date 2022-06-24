47a74e43011d200e729d2b0be201daa4b10ee0de
Mohit Passan <mohitpassan@gmail.com>
2022-06-25 03:18:14 +0530

    Added option for parse mode while sending a message

--- telegram/sendMessage.js
+++ telegram/sendMessage.js
@@ -1,9 +1,9 @@
 const axios = require("axios");
 
-const sendMessage = async (text) => {
+const sendMessage = async (text, parseMode) => {
     const encodedText = encodeURIComponent(text);
     try {
-        await axios.get(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage?chat_id=${process.env.CHAT_ID}&text=${encodedText}`);
+        await axios.get(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage?chat_id=${process.env.CHAT_ID}&text=${encodedText}${parseMode ? `&parse_mode=${parseMode}` : ''}`);
         console.log("Message sent: ", text);
     }
     catch (err) {
