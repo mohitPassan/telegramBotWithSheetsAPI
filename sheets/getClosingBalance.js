54a5938ac8e210bc28b53f97c83fe3647bfbcbab
Mohit Passan <mohitpassan@gmail.com>
2022-07-02 17:53:11 +0530

    Checking if closing balance is defined or not

--- sheets/getClosingBalance.js
+++ sheets/getClosingBalance.js
@@ -1,19 +1,21 @@
 const getClosingBalance = async (gsapi, month, appendRes) => {
-    const lastAppendedCell = appendRes.data.updates.updatedRange.split(':')[1];
-    const rowNumber = lastAppendedCell.replace( /^\D+/g, '');
+    const lastAppendedCell = appendRes.data.updates.updatedRange.split(":")[1];
+    const rowNumber = lastAppendedCell.replace(/^\D+/g, "");
 
     try {
         const closingBalance = await gsapi.spreadsheets.values.get({
             spreadsheetId: process.env.SPREADSHEET_ID,
-            range: `${month}!F${rowNumber}`
+            range: `${month}!F${rowNumber}`,
         });
-    
+
+        if (!closingBalance || !closingBalance[0] || !closingBalance[0][0]) {
+            return "No closing balance available. Please enter initial value.";
+        }
+
         return closingBalance.data.values[0][0];
-    }
-    catch(error) {
+    } catch (error) {
         throw new Error(`Error while getting closing balance\n${error}`);
     }
+};
 
-}
-
-module.exports = { getClosingBalance }
+module.exports = { getClosingBalance };
