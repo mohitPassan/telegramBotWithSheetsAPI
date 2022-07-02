0275a9739fbe59e7bb65abb6f4140e5be35fdc90
Mohit Passan <mohitpassan@gmail.com>
2022-07-03 03:22:11 +0530

    Fixed error where closing balance was not being fetched

--- sheets/getClosingBalance.js
+++ sheets/getClosingBalance.js
@@ -3,16 +3,19 @@ const getClosingBalance = async (gsapi, month, appendRes) => {
     const rowNumber = lastAppendedCell.replace(/^\D+/g, "");
 
     try {
-        const closingBalance = await gsapi.spreadsheets.values.get({
+        const res = await gsapi.spreadsheets.values.get({
             spreadsheetId: process.env.SPREADSHEET_ID,
             range: `${month}!F${rowNumber}`,
         });
 
-        if (!closingBalance || !closingBalance[0] || !closingBalance[0][0]) {
+        const values = res.data.values;
+
+        if (!values || !values[0] || !values[0][0]) {
             return "No closing balance available. Please enter initial value.";
         }
 
-        return closingBalance.data.values[0][0];
+        const closingBalance = values[0][0];
+        return closingBalance;
     } catch (error) {
         throw new Error(`Error while getting closing balance\n${error}`);
     }
