508b31264b714bfb666f53763696322a17c0ce96
Mohit Passan <mohitpassan@gmail.com>
2022-07-16 18:53:39 +0530

    Created script to setup dev
    - Created a script that open three terminals:
    1. nodemon
    2. ngrok
    3. Node script that sets the webhook
    - Will have to figure out how to reset the webhook once I close everything

---
+++ dev/setup.js
@@ -0,0 +1,31 @@
+const { exec } = require("child_process");
+const axios = require("axios");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const setup = async () => {
+    console.log("Setting up webhook");
+    try {
+        const res = await axios.get("http://localhost:4040/api/tunnels");
+        const data = res.data;
+
+        if (!data || data.tunnels.length === 0) {
+            console.log("Tunnel not started. Retrying in 2 seconds");
+            setTimeout(setup, 2000);
+        }
+
+        const publicUrl = data.tunnels[0].public_url;
+        console.log(`Public URL is: ${publicUrl}`);
+        console.log(`Bot token is: ${process.env.BOT_TOKEN}`);
+
+        await axios.get(
+            `https://api.telegram.org/bot${process.env.BOT_TOKEN}/setWebhook?url=${publicUrl}/api/${process.env.BOT_TOKEN}`
+        );
+        console.log("Webhook is set successfully");
+    } catch (error) {
+        console.log(`Error in setup.js: ${error}`);
+    }
+};
+
+setup();
